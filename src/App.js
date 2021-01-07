48090c215ff58651da6e5165ef4fafa9abfab7bd
stuartErnstsen <stuarternstsen@gmail.com>
2021-01-07 14:40:58 -0700

    Completed starting JSX structure for header and responsive CSS for header, logo, nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuView: false
+    }
+  }
+
+  toggleMenu = () => {
+    this.setState({ menuView: !this.state.menuView })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <div id="header-inner-container">
+            <img src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="logo"></img>
+            <nav>
+              <ul>
+                <li>SERVICES</li>
+                <li>PORTFOLIO</li>
+                <li>ABOUT</li>
+                <li>TEAM</li>
+                <li>CONTACT</li>
+              </ul>
+              <div id="header-menu-button" onClick={this.toggleMenu}>MENU</div>
+            </nav>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
